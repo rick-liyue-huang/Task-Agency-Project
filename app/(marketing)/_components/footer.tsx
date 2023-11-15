2b51220d002108cc197957eaac7f3436027f1955
Rick Huang <rick.liyue.huang@gmail.com>
2023-11-15 21:48:24 +1100

    feat: complete the marketing page layout

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button';
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 w-full p-4 border-t bg-amber-50">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-around w-full">
+          <Button size="sm" variant={'ghost'}>
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant={'ghost'}>
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: 'Task Agency',
+  description:
+    'We are a task management platform that helps you and your team to work',
+};
